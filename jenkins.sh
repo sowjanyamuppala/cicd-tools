956267c27ff598cb6380805f92e53c64a8bbfd37
Sowjanya-Dev-secops <sowji.muppala@gmail.com>
2026-03-10 13:34:01 +0530

    cicd-tools

--- jenkins.sh
+++ jenkins.sh
@@ -19,6 +19,7 @@ xfs_growfs /home
 curl -fsSL https://pkg.jenkins.io/redhat-stable/jenkins.repo \
 -o /etc/yum.repos.d/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+yum install fontconfig java-21-openjdk -y
 yum repolist | grep jenkins
 yum install jenkins -y
 systemctl daemon-reload
