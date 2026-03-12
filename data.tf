76c98ddeb92fa1bd1d455227dbedf6c2990aad1d
Sowjanya-Dev-secops <sowji.muppala@gmail.com>
2026-03-12 10:10:13 +0530

    jenkins

--- data.tf
+++ data.tf
@@ -21,22 +21,22 @@ data "aws_ami" "joindevops" {
 }
 
 
-# data "aws_ami" "sonarqube" {
-#   most_recent = true
-#   owners      = ["679593333241"] # Solve DevOps
+data "aws_ami" "sonarqube" {
+  most_recent = true
+  owners      = ["679593333241"] # Solve DevOps
 
-#   filter {
-#     name   = "name"
-#     values = ["SolveDevOps-SonarQube-Server-Ubuntu24.04-*"]
-#   }
+  filter {
+    name   = "name"
+    values = ["SolveDevOps-SonarQube-Server-Ubuntu24.04-*"]
+  }
 
-#   filter {
-#     name   = "virtualization-type"
-#     values = ["hvm"]
-#   }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 
-#   filter {
-#     name   = "architecture"
-#     values = ["x86_64"]
-#   }
-# }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
