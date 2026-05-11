a0064326d15c19802db4ffa5baf78a39e2d32381
Sowjanya-Dev-secops <sowji.muppala@gmail.com>
2026-05-11 09:00:18 +0400

    github

--- provider.tf
+++ provider.tf
@@ -7,8 +7,8 @@ terraform {
   }
 
   backend "s3" {
-    # bucket = "remote-state-sowji-dev1"
-    bucket = "sgr-remote-state-dev"
+    bucket = "remote-state-sowji-dev1"
+    #bucket = "sgr-remote-state-dev"
     key    = "roboshop-dev-jenkins"
     region = "us-east-1"
     use_lockfile = true
--- variables.tf
+++ variables.tf
@@ -8,15 +8,15 @@ variable "environment" {
 
 variable "zone_name" {
   type        = string
-  default     = "sgrdevsecops.fun"
-  # default     = "msdevsecops.fun"
+  # default     = "sgrdevsecops.fun"
+  default     = "msdevsecops.fun"
   description = "description"
 }
 
 variable "zone_id" {
   type        = string
-  # default     = "Z02877213Q6KXL0HOGN3V"
-  default = "Z0732355102QE6GB8XDYY"
+  default     = "Z02877213Q6KXL0HOGN3V"
+  #default = "Z0732355102QE6GB8XDYY"
   description = "description"
 }
 
